8dfe9bd0f4e19b457179b6007745cd6cb4e04721
WaLid-Jbir <anawalid737@gmail.com>
2025-05-25 23:54:22 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import helmet from 'helmet';
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// Server listening
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
