0298d9992f31b863a37a491c0baa85a1f61de338
WaLid-Jbir <anawalid737@gmail.com>
2025-06-05 14:53:24 +0100

    Verify email account with the code sent to user email

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { register, login, logout, sendVerificationCode } from '../controllers/auth.controller.js';
+import { register, login, logout, sendVerificationCode, verifyVerificationCode } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
@@ -7,6 +7,7 @@ router.post('/register', register);
 router.post('/login', login);
 router.post('/logout', logout);
 
-router.patch('/verify-email', sendVerificationCode);
+router.patch('/send-verification-code', sendVerificationCode);
+router.patch('/verify-verification-code', verifyVerificationCode);
 
 export default router;
