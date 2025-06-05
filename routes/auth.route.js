6d828785c448ffbcd6355f421eeb320ff6f62e1f
WaLid-Jbir <anawalid737@gmail.com>
2025-06-05 15:11:29 +0100

    Adding a middleware to check if the user is authenticated to access some functionalities

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import { register, login, logout, sendVerificationCode, verifyVerificationCode } from '../controllers/auth.controller.js';
+import { identifier } from '../middlewares/identification.js';
 
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
-router.post('/logout', logout);
+router.post('/logout', identifier, logout);
 
-router.patch('/send-verification-code', sendVerificationCode);
-router.patch('/verify-verification-code', verifyVerificationCode);
+router.patch('/send-verification-code', identifier, sendVerificationCode);
+router.patch('/verify-verification-code', identifier, verifyVerificationCode);
 
 export default router;
