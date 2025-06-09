da0628f7e427a0a410b0ef7f4458217a742ae729
WaLid-Jbir <anawalid737@gmail.com>
2025-06-09 18:08:19 +0100

    Reset the password by verifiying the code send to user email

--- middlewares/validator.js
+++ middlewares/validator.js
@@ -41,3 +41,13 @@ export const changePasswordSchema = Joi.object({
         .required()
         .pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{8,}$')),
 });
+
+export const forgotPasswordSchema = Joi.object({
+    email: Joi.string().min(6).max(60).required().email({
+        tlds: { allow: ['com', 'net'] },
+    }),
+    providedCode: Joi.number().required(),
+    newPassword: Joi.string()
+    .required()
+    .pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{8,}$')),
+});
