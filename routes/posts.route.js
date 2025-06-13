3f0fd5657504ea5116ac2af31dab669e39d32343
WaLid-Jbir <anawalid737@gmail.com>
2025-06-13 12:30:55 +0100

    Update post informations by it's id

--- routes/posts.route.js
+++ routes/posts.route.js
@@ -2,14 +2,16 @@ import express from "express";
 import { 
     getAllPosts,
     createPost,
-    getSinglePost
+    getSinglePost,
+    updatePost
 } from "../controllers/posts.controller.js";
 import { identifier } from '../middlewares/identification.js';
 
 const router = express.Router();
 
 router.get("/all-posts", getAllPosts );
-router.post("/create-post", identifier, createPost );
 router.get("/single-post", getSinglePost );
+router.post("/create-post", identifier, createPost );
+router.put("/update-post", identifier, updatePost );
 
 export default router;
